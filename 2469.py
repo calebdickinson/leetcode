8ddda2f8c14cf6d31f094465788d0b789007972f
Caleb Dickinson <calebisaacdickinson@gmail.com>
2023-05-14 17:38:54 -0600

    Update 2469.py

--- 2469.py
+++ 2469.py
@@ -1,5 +1,2 @@
 class Solution:
-    def convertTemperature(self, celsius: float) -> List[float]:
-        a=celsius+273.15
-        b=celsius*1.80+32
-        return([a,b])
+    def convertTemperature(a,d:0)->[]:b=d+273.15;c=d*1.80+32;return([b,c])
